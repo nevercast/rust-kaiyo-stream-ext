c70419df1026a3ed0375eb86321fa4d0d1e2ed64
Josh Lloyd <834342+nevercast@users.noreply.github.com>
2023-03-01 04:22:32 +1300

    Increase top model count to 7

--- static/app.js
+++ static/app.js
@@ -1,7 +1,7 @@
 const e = React.createElement;
 const DateTime = luxon.DateTime;
 const CONNECTION_LOST_TIMEOUT = 10;
-const POPULAR_MODELS_LIMIT = 3; // Show this many most popular models.
+const POPULAR_MODELS_LIMIT = 7; // Show this many most popular models.
 
 const Sidebar = ({ currentModel, previousModels, popularModels }) => {
     const showPreviousModels = previousModels && previousModels.length > 0;
